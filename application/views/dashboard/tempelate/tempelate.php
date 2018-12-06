3335024405b71f5fc6c85af5c43482385d697528
jihadul4kbar <jihadul4kbar@gmail.com>
2018-12-06 15:23:54 +0700

    add icon

--- application/views/dashboard/tempelate/tempelate.php
+++ application/views/dashboard/tempelate/tempelate.php
@@ -119,24 +119,6 @@
                     </div>
                   </li>
                   <li class="nav-item dropdown">
-                    <a href="javascript:void(0)" class="nav-link" data-toggle="dropdown"><i class="fe fe-file"></i> Pages</a>
-                    <div class="dropdown-menu dropdown-menu-arrow">
-                      <a href="./profile.html" class="dropdown-item ">Profile</a>
-                      <a href="./login.html" class="dropdown-item ">Login</a>
-                      <a href="./register.html" class="dropdown-item ">Register</a>
-                      <a href="./forgot-password.html" class="dropdown-item ">Forgot password</a>
-                      <a href="./400.html" class="dropdown-item ">400 error</a>
-                      <a href="./401.html" class="dropdown-item ">401 error</a>
-                      <a href="./403.html" class="dropdown-item ">403 error</a>
-                      <a href="./404.html" class="dropdown-item ">404 error</a>
-                      <a href="./500.html" class="dropdown-item ">500 error</a>
-                      <a href="./503.html" class="dropdown-item ">503 error</a>
-                      <a href="./email.html" class="dropdown-item ">Email</a>
-                      <a href="./empty.html" class="dropdown-item ">Empty page</a>
-                      <a href="./rtl.html" class="dropdown-item ">RTL mode</a>
-                    </div>
-                  </li>
-                  <li class="nav-item dropdown">
                     <a href="./form-elements.html" class="nav-link"><i class="fe fe-check-square"></i> Forms</a>
                   </li>
                   <li class="nav-item">
@@ -195,7 +177,7 @@
               </div>
             </div>
             <div class="col-12 col-lg-auto mt-3 mt-lg-0 text-center">
-              Copyright © 2018 <a href=".">Tabler</a>. Theme by <a href="https://codecalm.net" target="_blank">codecalm.net</a> All rights reserved.
+              Copyright © 2018 <a href=".">WebGIS</a>. All rights reserved.
             </div>
           </div>
         </div>
